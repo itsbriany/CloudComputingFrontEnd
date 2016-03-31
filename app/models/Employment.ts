ec72b11d8ee8bcedcc080ec1267eaa60bf0b9a95
Brian Yip <itsbriany@gmail.com>
2016-03-30 23:20:15 -0300

    Added the missing employment class

---
+++ app/models/Employment.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by itsbriany on 2016-03-30.
+ */
+
+export class Employment {
+    salary: number;
+    start_date: string;
+    
+    constructor(salary: number, start_date: string) {
+        this.salary = salary;
+        this.start_date = start_date;
+    }
+}
